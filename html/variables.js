93de7eec6bc591813b765ecc6f3df4f2b2361f1b
Veronicact1 <123936150+Veronicact1@users.noreply.github.com>
2023-02-06 14:44:32 +0100

    Monday

---
+++ html/variables.js
@@ -0,0 +1,9 @@
+var alert1 = 5;
+// permitido
+var alert1 = '';
+let alert2 = 5;
+// no permitido porque ya se declaró antes
+const alert3=15;
+alert2=15;
+console.log (alert1);
+console.log (alert2);
