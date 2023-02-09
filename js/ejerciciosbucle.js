f5e4f6d0c2dd5d8c164b229567de43b007b4d82c
Veronicact1 <123936150+Veronicact1@users.noreply.github.com>
2023-02-09 14:45:39 +0100

    thuersday

---
+++ js/ejerciciosbucle.js
@@ -0,0 +1,5 @@
+let numeroentero=prompt("dame un numero");
+for (let wk=numeroentero; wk>=0; wk--)
+if(wk>0){
+document.write(wk +",") }
+else{document.write(wk)}s
---
+++ js/exercice.js
@@ -0,0 +1,38 @@
+// let numeroentero=prompt("dame un numero");
+// for (let wk=numeroentero; wk>=0; wk--)
+// if(wk>0){
+// document.write(wk +",") }
+// else{document.write(wk)}
+
+// let contador2 =0;
+// while (contador2 <= 50){
+// console.log(contador2)
+// contador2++;}    ----CORECT
+
+// let tabla = 0;
+// while ( tabla <=10 )
+// {
+// document.write (" 5 x " + tabla + " = " + tabla * 5 + "<br>");
+// tabla++;
+// } -----  CORECT greseala ; dupa (tabla<=10) nu se pune.----
+
+
+// let nombre=prompt("escribe tu nombre");
+// let j=0;
+// while ( j<10)
+// {
+// document.write(nombre+"<br>");
+// j++;
+// }------ CORECT
+
+
+// let palabra=prompt("Dame una palabra");
+// let r=numeroentero;
+// while ( r>=0)
+// {if (r>0){
+//   console.log(r + 1 + "." + palabra);}
+//   else{document.write(r)}
+//   r++;
+// } ----NU AM INCERCAT EX 11 PAG171
+
+
